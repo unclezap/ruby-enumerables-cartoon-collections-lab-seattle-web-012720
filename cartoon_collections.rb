8d8e23ce26880670d28499db28aeae161c8280dc
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2020-01-21 13:39:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,10 @@ def roll_call_dwarves(dwarves)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteers)# code an argument here
   # Your code here
+  
+  planeteers.
 end
 
 def long_planeteer_calls# code an argument here
