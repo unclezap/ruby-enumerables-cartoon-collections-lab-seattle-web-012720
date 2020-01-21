14e850505bf43fddadd4e7651323730ac5647598
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2020-01-21 13:40:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,8 @@ end
 def summon_captain_planet(planeteers)# code an argument here
   # Your code here
   
-  planeteers.
+  planeteers.map do |name|
+    
 end
 
 def long_planeteer_calls# code an argument here
