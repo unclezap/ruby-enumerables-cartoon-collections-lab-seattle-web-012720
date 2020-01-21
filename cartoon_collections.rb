00488deb2df77b7cceeae7029acc32085d965bd9
Zebadiah Blackwell <zeb.blackwell@gmail.com>
2020-01-21 13:36:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   dwarves.each_with_index do |name,i|
-    
-  
+    puts i + " #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
